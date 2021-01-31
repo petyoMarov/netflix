0ef7657d9e1dafe6433522df0a3dd3a1048cbbb2
Petyo <petyomarov@gmail.com>
2021-01-31 21:01:32 +0200

    testing setup and player test

--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,4 @@ export { default as Form } from './form';
 export { default as Profiles } from './profiles';
 export { default as Loading } from './loading';
 export { default as Card } from './card';
-export { default as PLayer } from './player';
+export { default as Player } from './player';
